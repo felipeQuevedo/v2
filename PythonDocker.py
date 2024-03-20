1e1d665f91e31b1cf0522f348ed2daacf3e55001
felipeQuevedo <felipequevedo3q@gmail.com>
2024-03-20 12:16:50 -0500

    Update PythonDocker.py

--- PythonDocker.py
+++ PythonDocker.py
@@ -1,4 +1,21 @@
+import http.server
+import socketserver
 import webbrowser
+
+# Configura el puerto en el que deseas ejecutar el servidor
+PORT = 6368
+
+# Define el controlador de solicitudes HTTP
+class MyHandler(http.server.SimpleHTTPRequestHandler):
+    pass
+
+# Configura el servidor en el puerto especificado
+with socketserver.TCPServer(("", PORT), MyHandler) as httpd:
+    print("Servidor HTTP activo en el puerto", PORT)
+    # Abre el navegador en la dirección del servidor
+    webbrowser.open_new_tab(f'http://localhost:{PORT}/holamundo.html')
+    # Inicia el servidor y espera las solicitudes
+    httpd.serve_forever()
 
 f = open('holamundo.html','w')
 
@@ -11,5 +28,6 @@ mensaje = """<html>
 
 f.write(mensaje)
 f.close()
+
 
 webbrowser.open_new_tab('holamundo.html')
